6c7462beea100a524ff96f24a83e620e249646a0
Mel Grosser <m.grosser@curasoft.de>
2022-01-24 11:24:58 +0100

    style update

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,8 @@ const matrix = ref<number[][]>(createValueMatrix(rows, numberOfDaysInMonth))
 const sumFirstMatrixRow = ref<number>(31)
 const sumSecondMatrixRow = ref<number>(31)
 const sumThirdMatrixRow = ref<number>(31)
+const mouseEventRow = ref<number>(-1)
+const mouseEventCol = ref<number>(-1)
 
 const tableHead = ['Vertrag', 'Leistung', ...Array.from(Array(numberOfDaysInMonth), (e, i) => i + 1), 'A']
 const firstRow = ['', 'Pflegeversicherung', ...createWeeKArray(5, numberOfDaysInMonth), '']
@@ -21,8 +23,6 @@ const fourthRow = ['', 'vormittags', ...Array.from(Array(numberOfDaysInMonth), (
 const fifthRow = ['SGB XI HH', 'LK5: Lagern/Betten']
 const sixthRow = ['', 'mittags', ...Array.from(Array(numberOfDaysInMonth), (e, i) => i + 1), '']
 const seventhRow = ['SGB XI HH', 'LK17: Kl. Besorgungen']
-
-// const sumFirstMatrixRow = computed(() => matrix.value[0].reduce((acc: number, curr: number) => acc + curr))
 
 function createValueMatrix(rows: number, columns: number): number[][] {
   return Array.from(Array(rows), () => new Array(columns).fill(1))
@@ -54,13 +54,29 @@ const sumOfMatrixRow = (matrixIndex: number) => {
 }
 
 const isSunday = (index: number, offset: number): string => {
-  if ((index + offset) % 7 === 0) return '!bg-neutral-200'
+  if ((index + offset) % 7 === 0) return 'bg-neutral-200'
   return ''
 }
 
 const isHoliday = (index: number, offset: number): string => {
-  if ((index - offset) === 1) return '!bg-green-200'
+  if ((index - offset) === 1) return 'bg-green-200'
   return ''
+}
+
+const checkSpecialDay = (index: number, offset: number, defaultColor = ''): string => {
+  if (isSunday(index, offset) !== '') return isSunday(index, offset)
+  if (isHoliday(index, offset) !== '') return isHoliday(index, offset)
+  return defaultColor
+}
+
+const handleMouseEnterEvent = (row: number, col: number) => {
+  mouseEventCol.value = col
+  mouseEventRow.value = row
+}
+
+const handleMouseLeaveEvent = () => {
+  mouseEventCol.value = -1
+  mouseEventRow.value = -1
 }
 
 const readMode = () => {
@@ -113,14 +129,14 @@ onUpdated(() => {
     >Verschieben</button>
   </div>
   <div class="w-full flex justify-center py-20 text-center tracking-wide">
-    <table :class="activeMode === 'read' && 'pointer-events-none'">
+    <table>
       <thead>
         <tr>
           <th
             v-for="(head, i) in tableHead"
             :key="head"
-            class="border border-gray-400 bg-gray-100 py-2 font-normal border-b-black border-b-2"
-            :class="isSunday(i, 4), isHoliday(i, 1)"
+            class="border border-gray-400 py-2 font-normal border-b-black border-b-2"
+            :class="isSunday(i, 4) === '' ? isHoliday(i, 1) === '' ? 'bg-gray-100' : isHoliday(i, 1) : isSunday(i, 4)"
           >{{ head }}</th>
         </tr>
       </thead>
@@ -154,8 +170,8 @@ onUpdated(() => {
             :class="isSunday(i, 6), isHoliday(i, -1)"
           >
             <input
-              v-if="i < matrix[0].length - 1"
               type="text"
+              :disabled="activeMode === 'read'"
               class="w-10 text-center py-2 px-2"
               v-model.number="matrix[0][i]"
               :class="isSunday(i, 6), isHoliday(i, -1)"
@@ -187,8 +203,8 @@ onUpdated(() => {
             :class="isSunday(i, 6), isHoliday(i, -1)"
           >
             <input
-              v-if="i < matrix[1].length - 1"
               type="text"
+              :disabled="activeMode === 'read'"
               class="w-10 text-center py-2 px-2"
               v-model.number="matrix[1][i]"
               :class="isSunday(i, 6), isHoliday(i, -1)"
@@ -207,16 +223,19 @@ onUpdated(() => {
             :class="isSunday(i, 4), isHoliday(i, 1)"
           >{{ item }}</td>
         </tr>
-        <tr>
+        <tr :class="mouseEventRow === 7 && '!bg-red-400'">
           <td v-for="item in seventhRow" :key="item" class="border border-gray-400 px-2">{{ item }}</td>
           <td
             v-for="(x, i) in matrix[2]"
             :key="i"
             class="border border-gray-400"
             :class="isSunday(i, 6), isHoliday(i, -1)"
+            @mouseenter="handleMouseEnterEvent(7, i)"
+            @mouseleave="handleMouseLeaveEvent"
           >
             <input
               type="text"
+              :disabled="activeMode === 'read'"
               class="w-10 text-center py-2 px-2"
               v-model.number="matrix[2][i]"
               :class="isSunday(i, 6), isHoliday(i, -1)"
